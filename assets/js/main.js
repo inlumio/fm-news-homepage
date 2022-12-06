fec2814e5774b62a0d6dbe97868bd9708a4ee1e5
inlumio <vladyslav.pjatkyvskyj@gmail.com>
2022-12-06 19:57:36 +0200

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,12 @@
 const navToggler = document.getElementById('nav-toggle');
+const navList = document.getElementById('nav-list');
+const navLink = document.querySelectorAll('.nav__link');
+
 navToggler.addEventListener('click', () => {
 	navToggler.classList.toggle('active');
+	navList.classList.toggle('active');
 });
+
+navLink.forEach(n => n.addEventListener('click', () => {
+	navToggler.classList.remove('active');
+}));
